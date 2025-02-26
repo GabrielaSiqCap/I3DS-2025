1586030cca2b74c4fbd341b738ca265889af8981
Gabriela <gabrielasiqueiracaputti@gmail.com>
2025-02-26 16:17:47 -0300

    Finalizando edição de css e app.jsx

--- 03-devlinks/src/App.jsx
+++ 03-devlinks/src/App.jsx
@@ -1,32 +1,62 @@
-import "./App.css"
+import "./App.css";
+import perfilDark from "./img/rapunzel.jpg";
+import perfilLight from "./img/jose.jpg";
 
-import Perfil from "./components/perfil/perfil"
-import Switch from "./components/switch/Switch"
-import Links from "./components/links/Links"
-import SocialLinks from "./components/socialLinks/SocialLinks"
-import Rodape from "./components/rodape/Rodape"
+import Perfil from "./components/perfil/perfil";
+import Switch from "./components/switch/Switch";
+import Links from "./components/links/Links";
+import SocialLinks from "./components/socialLinks/SocialLinks";
+import Rodape from "./components/rodape/Rodape";
+import { useState } from "react";
 
 const App = () => {
-  return (
-    <div id="App" className="">
+  const [isLight, setIsLight] = useState(true);
 
-    <Perfil/>
-    <Switch/>
+  const troca = () => {
+    setIsLight(!isLight);
+  };
+
+  return (
+    <div id="App" className={isLight ? "light" : ""}>
+
+<div id="container">
+
+    <Perfil fotoPerfil={isLight ? perfilLight : perfilDark}>@siqueiracaputti</Perfil>
+
+    <Switch troca={troca} isLight={isLight} />
+
+<div id="links">
     <ul>
-    <Links/>
-    <Links/>
-    <Links/>
-    <Links/>
+    <Links link={"https://github.com/GabrielaSiqCap"}>GitHub</Links>
+    <Links link={"https://www.instagram.com/siqueiracaputti/"}>Instagram</Links>
+    <Links link={"https://br.pinterest.com/siqueiracaputti/"}>Pinterest</Links>
+    <Links link={"https://open.spotify.com/user/31ztpuy6xtjfys54iyinasag662a"}>Spotify</Links>
     </ul>
+    </div>
 
 <div id="socialLinks">
-  <SocialLinks />
-  <SocialLinks />
-  <SocialLinks />
-  <SocialLinks />
+  <SocialLinks 
+  link={'https://github.com/GabrielaSiqCap'}
+  icon={"logo-github"}
+  />
+  <SocialLinks 
+  link={'https://www.instagram.com/siqueiracaputti/'}
+  icon={"logo-instagram"}
+  />
+  <SocialLinks 
+  link={'https://br.pinterest.com/siqueiracaputti/'}
+  icon={"logo-pinterest"}
+  />
+  <SocialLinks 
+  link={'https://open.spotify.com/user/31ztpuy6xtjfys54iyinasag662a'}
+  icon={"play-circle-outline"}
+  />
 </div>
-<Rodape/>
+
+<Rodape>gabriela</Rodape>
+
     </div>
+  </div>
   )
 }
 
--- 03-devlinks/src/components/switch/Switch.jsx
+++ 03-devlinks/src/components/switch/Switch.jsx
@@ -1,12 +1,14 @@
 import styles from './Switch.module.css'
 
-const Switch = () => {
+const Switch = ({troca, isLight}) => {
   return (
-    <div id={styles.switch}>
+    <div className={isLight ? styles.light : ""}>
+    <div onClick={troca} id={styles.switch}>
       <button></button>
       <span></span>
     </div>
-  )
-}
+    </div>
+  );
+};
 
 export default Switch
