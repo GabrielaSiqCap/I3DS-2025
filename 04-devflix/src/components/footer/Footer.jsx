61410e51436ea2aee8c38c23cc48736786a71540
Gabriela <gabrielasiqueiracaputti@gmail.com>
2025-03-11 14:50:25 -0300

    Melhorando MovieCard com busca e mapeamento

---
+++ 04-devflix/src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import styles from "./Footer.module.css"
+
+const Footer = (props) => {
+  return (
+    <footer>
+      <p>Feito com ❤️ por
+        <a href={props.devLink}>{props.devName}</a></p>
+    </footer>
+  )
+}
+
+export default Footer
